6cb564f95122c289c5182d43d4dd47e1698daf93
Soyab <itsmdsoyab18@gmail.com>
2026-06-22 16:13:01 +0530

    Understanding TypesOfVariables

--- Java/Variables.java
+++ Java/Variables.java
@@ -11,9 +11,23 @@ class Variables {
             }
         }
     }
+    static void TypesOfVariables(){
+        int a=10;
+        float b=20.5f;
+        double c=30.5;
+        char d='A'; //char stores single character as a number eg. A=65, B=66, C=67 lowercase a=97, b=98, c=99
+        String e="Hello";
+        System.out.println("Integer: " + a);
+        System.out.println("Float: " + b);
+        System.out.println("Double: " + c);
+        System.out.println("Character: " + d);
+        System.out.println("String: " + e);
+
+    }
     public static void main(String[] args) {
         output();
         LeftTrianglePattern();
+        TypesOfVariables();
     }
     
 }
