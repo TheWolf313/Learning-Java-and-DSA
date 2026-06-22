0b63fde2f970924a51806e968247034cf52fa2c6
Soyab <itsmdsoyab18@gmail.com>
2026-06-22 16:07:06 +0530

    Understanding Pattern printing

--- Java/Variables.java
+++ Java/Variables.java
@@ -1,5 +1,19 @@
 class Variables {
-    public static void main(String[] args) {
+    static void output(){
         System.out.println("Hello World!");
     }
+    static void LeftTrianglePattern(){
+        for(int i=0;i<4;i++){
+            System.out.println();
+            for(int j=0;j<4;j++){
+                if(j<=i)
+                System.out.print("* ");
+            }
+        }
+    }
+    public static void main(String[] args) {
+        output();
+        LeftTrianglePattern();
+    }
+    
 }
